33c4cb1156fd79eb52e7ba6fae55237272724411
Björn Palmqvist <bjorn@aidium.se>
2015-09-30 11:23:49 +0200

    Bas för TDD kator

---
+++ kata/src/main/java/se/its/kata/App.java
@@ -0,0 +1,13 @@
+package se.its.kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ kata/src/test/java/se/its/kata/AppTest.java
@@ -0,0 +1,16 @@
+package se.its.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class AppTest 
+{
+
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
