13eff1f1ee249cb69ee1c100d80e4064c737f3c4
Lars-Henrik Snow <lars@harhund.com>
2015-12-15 21:51:08 +0100

    canary

--- commas/app/app.spec.js
+++ commas/app/app.spec.js
@@ -1,1 +1,11 @@
+describe('Comma separator filter', function() {
+  var filter;
+  beforeEach(module('kata'));
+  beforeEach(inject(function(commaSeparatedFilter){
+    filter = commaSeparatedFilter;
+  }));
 
+  it("canary", function() {
+    expect(true).toBe(true);
+  });
+});
